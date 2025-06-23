873d6b45721738df6063b4cda3e6ce1713c84bed
andregustavo-plank <andre.gustavo@joinplank.com>
2025-06-23 13:49:43 -0300

    Adding docker compose

--- mcp-server-vector/server.py
+++ mcp-server-vector/server.py
@@ -11,18 +11,18 @@ mcp = FastMCP("Demo")
 @mcp.tool()
 def add_embedding_columns_to_table(table_name: str, embedding_column_name: str, connection_string: str) -> int:
     """Add an embedding column to a table in a PostgreSQL database"""
-    try:
-         # Use 'vector' as the column type for embeddings
-        add_column_to_table(connection_string, table_name, embedding_column_name)
-        return "The embedding column has been added to the table"
-    except Exception as e:
-        return f"Error adding embedding column to table {table_name} in database {connection_string}: {e}"
+    # try:
+    #      # Use 'vector' as the column type for embeddings
+    #     add_column_to_table(connection_string, table_name, embedding_column_name)
+    #     return "The embedding column has been added to the table"
+    # except Exception as e:
+    #     return f"Error adding embedding column to table {table_name} in database {connection_string}: {e}"
 
 @mcp.tool()
 def generate_embedding(column_name_from: str, column_name_to: str, table_name: str, connection_string: str, openai_api_key: str) -> int:
     """Generate embeddings for a column and store them in another column using postgres_provider"""
-    result = process_embeddings(connection_string, openai_api_key, table_name, column_name_from, column_name_to)
-    return result
+    # result = process_embeddings(connection_string, openai_api_key, table_name, column_name_from, column_name_to)
+    # return result
 
 @mcp.tool()
 def health_check(test_string: str) -> int:
